a766c168a9ab9834193b3001b3150a27cd204073
Boonyon Kim <initialkbn@gmail.com>
2018-04-26 23:45:55 +0900

    Start inertialization

---
+++ Source/Inertialization/InertializationGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InertializationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INERTIALIZATION_API AInertializationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
